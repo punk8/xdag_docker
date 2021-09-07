aed5b4bbb03fdb8005cc70d81547f6180de09d4d
punk <punk8sang@gmail.com>
2021-09-07 13:36:27 +0800

    modify

--- ci/ci.sh
+++ ci/ci.sh
@@ -1,21 +1,66 @@
-echo "hello"
+echo "rebuild xdagj"
+my_home=$(cd "$(dirname "$0")"; pwd)
+#if [ $# -ne 1 ]; then
+#        echo "[`date`] usage : ./build.sh {[module]}"
+#        exit 1
+#fi
+#CMD=$1
 
-NODE_NUM=$1
-NODE_POOL=127.0.0.1
-NODE_PORT=800
-POOL_PORT=801
-RPC_PORT=802
+branch=${2:-'develop'}
 
-if [ $NODE_NUM -ge 10 ];then
-  echo "NODE_NUM need less than 10"
-  exit
+echo $branch
+
+#if [ $CMD != "release" ] && [ $CMD != "java" ] && [ $CMD != "vue" ] && [ $CMD != "toolkit" ]; then
+#       echo "[`date`] usage : ./build.sh {release|java|vue|toolkit}"
+#       echo "aaaaa"
+#       exit 1
+#fi
+
+MODULE=$1
+echo $MODULE
+
+type git
+ret=$?
+if [ "$ret" != 0 ]; then
+        echo "[`date`] WARNING : git not install."
+        exit 1
 fi
 
-echo "" > netdb-testnet.txt
-echo "" > netdb-white-testnet.txt
-for n in $(seq 1 $NODE_NUM)
-do
-  echo "$NODE_POOL:$NODE_PORT$n" >> netdb-testnet.txt
-  echo "$NODE_POOL:$NODE_PORT$n" >> netdb-white-testnet.txt
-done
+command -v mvn
+ret=$?
+if [ "$ret" != 0 ]; then
+    echo "[`date`] WARNING : maven not install."
+    exit 1
+fi
+
+cd $MODULE
+git checkout $branch
+# 1.2) pull newest code
+git pull origin $branch
+
+ret=$?
+if [ "$ret" != 0 ]; then
+    echo "[`date`] WARNING : git pull failed."
+    exit 1
+fi
+
+mvn clean package -Dmaven.test.skip=true
+ret=$?
+if [ "$ret" != 0 ]; then
+        echo "[`date`] WARNING : compile fail."
+        exit 1
+fi
+
+echo $MODULE
+
+# 切回工作目录
+cd $my_home
+if [ ! -d "$my_home/xdag_pool" ]; then
+  mkdir $my_home/xdag_pool
+fi
+scp $MODULE/target/xdagj*.jar $my_home/xdag_pool
+scp $MODULE/script/xdag.sh $my_home/xdag_pool
+scp $MODULE/src/main/resources/xdag-*.config $my_home/xdag_pool
+scp $MODULE/src/main/resources/netdb-*.txt $my_home/xdag_pool
+
 
